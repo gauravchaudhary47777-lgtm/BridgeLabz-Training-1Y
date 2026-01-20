3e4d239d9854361290b00cf46849472d8c3f13d0
gauravchaudhary47777-lgtm <gauravchaudhary47777@gmail.com>
2026-01-20 10:18:34 +0530

    Update student.java

--- practice programs/student.java
+++ practice programs/student.java
@@ -1,17 +1,17 @@
 public class Student {
-
-    static void displayDetails(int id, String name, int age) {
+    static void displayDetails(int id, String name, int age, String status) {  
         System.out.println("ID: " + id);
         System.out.println("Name: " + name);
         System.out.println("Age: " + age);
+        System.out.println("Status: " + status);
     }
 
     public static void main(String[] args) {
-
         int id = 101;
         String name = "Gaurav";
         int age = 19;
+        String status = "Active";
 
-        displayDetails(id, name, age);
+        displayDetails(id, name, age, status);  
     }
 }
