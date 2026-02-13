8c5c2e342e8e22be4eefbc8f4243a67254c2c184
gauravchaudhary47777-lgtm <gauravchaudhay47777@gamil.com>
2026-02-13 11:48:21 +0530

    change in program

--- practice-programs/ConstructorDemo.java
+++ practice-programs/ConstructorDemo.java
@@ -11,10 +11,16 @@ Public ConstructorDemo(int id, String name,int age){
   this.name= name;
   this.age=age;
 }
+ void Display(){
+	System.out.println("ID:" +id);
+	System.out.println("Name" +name);
+	System.out.println("Age" +age);
+ }
  
  public static void main(String args[]){
-    ConstructorDemo cd= new ConstructorDemo(1,"jay",20);
-	System.out.println(cd);
-  
+    ConstructorDemo cd= new ConstructorDemo(1,"Gaurav",26);
+	System.out.println(cd); 
+	cd.display();
 }
+
 }
